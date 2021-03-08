2e3cb1748912635ae73a102c36ec64f2224362bf
sataapon <mr.sataapon@gmail.com>
2021-03-08 13:19:22 +0700

    main.go run successfully

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 
-	dataset "github.com/Sataapon/contacts/tickets"
+	"github.com/Sataapon/contacts/channel"
+	"github.com/Sataapon/contacts/graph"
+	"github.com/Sataapon/contacts/mapping"
+	"github.com/Sataapon/contacts/source"
 )
 
 func main() {
@@ -12,6 +15,19 @@ func main() {
 
 func run() {
 	path := "dataset/contacts.json"
-	dataset := dataset.New(path)
-	fmt.Println(len(dataset.Tickets))
+
+	dataset := source.New(path)
+
+	emailMapping := mapping.NewEmail(dataset)
+	phoneMapping := mapping.NewPhone(dataset)
+	OrderIdMapping := mapping.NewOrderId(dataset)
+
+	graph := graph.New(dataset.Length())
+	graph.AddEdges(dataset, emailMapping, channel.Email)
+	graph.AddEdges(dataset, phoneMapping, channel.Phone)
+	graph.AddEdges(dataset, OrderIdMapping, channel.OrderId)
+
+	fmt.Println(graph.Adj()[1])
+	fmt.Println(graph.Adj()[2458])
+	fmt.Println(graph.Adj()[476346])
 }
